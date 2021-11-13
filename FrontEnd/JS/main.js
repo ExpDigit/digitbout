8e2ebfb069849d5d9c35e2192f85d2788118d5ad
EstySD <sergeevdns@gmail.com>
2021-11-13 23:23:29 +0500

    basket sdelalb

--- FrontEnd/JS/main.js
+++ FrontEnd/JS/main.js
@@ -48,3 +48,27 @@ function showCheckboxes3() {
     expanded = false;
   }
 }
+
+
+// Number
+let minusBtn = document.getElementById("minus-btn");
+let count = document.getElementById("count");
+let plusBtn = document.getElementById("plus-btn");
+
+let countNum = 0;
+count.innerHTML = countNum;
+
+minusBtn.addEventListener("click", () => {
+	countNum -= 1;
+    if (countNum<0)
+    {
+        countNum=0;
+    }
+	count.innerHTML = countNum;
+});
+
+plusBtn.addEventListener("click", () => {
+	countNum += 1;
+	count.innerHTML = countNum;
+});
+
