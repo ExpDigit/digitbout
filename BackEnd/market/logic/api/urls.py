3386a788c558c1c9db2a14167e973150192e0bba
Arhimage <sherst.pavel@yandex.ru>
2021-11-13 23:02:24 +0500

    Бот upd

--- BackEnd/market/logic/api/urls.py
+++ BackEnd/market/logic/api/urls.py
@@ -1,6 +1,6 @@
 from rest_framework import routers, viewsets
 from django.urls import path, include
-from .viewsets import StockViewSet, UserViewSet, CategoryViewSet, OrderViewSet, StockDetail, BotDataViewSet
+from .viewsets import StockViewSet, UserViewSet, CategoryViewSet, OrderViewSet, StockDetail, BotDataDetail
 from rest_framework.urlpatterns import format_suffix_patterns
 
 router = routers.DefaultRouter()
@@ -8,12 +8,12 @@ router.register(r'users', UserViewSet)
 router.register(r'cats', CategoryViewSet)
 router.register(r'orders', OrderViewSet)
 router.register(r'stock', StockViewSet)
-router.register(r'bot', BotDataViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
     path('edit-stock/', StockDetail.as_view()),
+    path('bot/', BotDataDetail.as_view()),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- BackEnd/market/logic/api/viewsets.py
+++ BackEnd/market/logic/api/viewsets.py
@@ -25,9 +25,9 @@ class StockViewSet(viewsets.ModelViewSet):
     queryset = Stock.objects.filter(published=False, publication_date__lte=now)
     serializer_class = StockSerializer
     
-class BotDataViewSet(viewsets.ModelViewSet):
-    queryset = BotData.objects.all()
-    serializer_class = BotDataSerializer
+# class BotDataViewSet(viewsets.ModelViewSet):
+#     queryset = BotData.objects.all()
+#     serializer_class = BotDataSerializer
 
 class StockDetail(APIView):
     def get_object(self, pk):
@@ -46,3 +46,27 @@ class StockDetail(APIView):
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class BotDataDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return BotData.objects.get(pk=pk)
+        except BotData.DoesNotExist:
+            raise Http404
+
+    def get(self, request, format=None):
+        bdata = BotData.objects.all()
+        serializer = BotDataSerializer(bdata, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = BotDataSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, format=None):
+        bdata = BotData.objects.get(user_id=request.data['user_id'])
+        bdata.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
